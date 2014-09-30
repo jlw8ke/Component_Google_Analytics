88a97b2beeaf7fb0d11d4ba3a72b86ad939c39aa
Justin Washington <jwashington@mobiquityinc.com>
2014-09-30 16:14:44 -0400

    google play services to most recent version #

--- app/add_analytics_dependency.sh
+++ app/add_analytics_dependency.sh
@@ -3,6 +3,7 @@ if grep -e 'com.google.android.gms:play-services' app/build.gradle; then
 		echo 'Google Play Services has already been added to the project'	
 else
 	echo 'Adding Google Play Servies dependency to the gradle file'
-	awk '/dependencies/{print;print "\t// Google Play Services\n\tcompile \"com.google.android.gms:play-services:5.+\"\n";next}1' app/build.gradle > tempfile
+	awk '/dependencies/{print;print "\t// Google Play Services\n\tcompile \"com.google.android.gms:play-services:5.2.08\"\n";next}1' app/build.gradle > tempfile
 	mv tempfile app/build.gradle
 fi
+
