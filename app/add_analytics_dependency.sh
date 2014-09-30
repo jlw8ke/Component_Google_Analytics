fd9fdedd9c20f59dafad6676bfd2c37e30491c35
Justin Washington <jwashington@mobiquityinc.com>
2014-09-30 16:01:16 -0400

    awk to add dependency to build.gradle

--- app/add_analytics_dependency.sh
+++ app/add_analytics_dependency.sh
@@ -3,6 +3,6 @@ if grep -e 'com.google.android.gms:play-services' app/build.gradle; then
 		echo 'Google Play Services has already been added to the project'	
 else
 	echo 'Adding Google Play Servies dependency to the gradle file'
-	sed '/dependencies/ a\ '$'\n compile \'com.google.android.gms:play-services:5.0.77\'\n' app/build.gradle > tempfile.txt
-	mv tempfile.txt app/build.gradle
+	awk '/dependencies/{print;print "\t// Google Play Services\n\tcompile \"com.google.android.gms:play-services:5.+\"\n";next}1' app/build.gradle > tempfile
+	mv tempfile app/build.gradle
 fi
