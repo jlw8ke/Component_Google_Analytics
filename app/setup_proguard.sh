f21cc111d4362b9d30c693023d22c8513ba33d79
Justin Washington <jwashington@mobiquityinc.com>
2014-10-02 15:27:47 -0400

    pro guard file prepared for google play services

--- app/setup_proguard.sh
+++ app/setup_proguard.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 proguard=$1
-echo "imma setup proguard now"
-echo "this is the proguard location: $proguard"
+echo "Setting up the proguard document"
+
+cat <<EOF >> $proguard
+-keep class * extends java.util.ListResourceBundle {
+	protected Object[][] getContents();
+}
+
+-keep public class com.google.android.gms.common.internal.safeparcel.SafeParcelable {
+	public static final *** NULL;
+}
+
+-keepnames @com.google.android.gms.common.annotation.KeepName class *
+-keepclassmembernames class * {
+	@com.google.android.gms.common.annotation.KeepName *;
+}
+
+-keepnames class * implements android.os.Parcelable {
+	public static final ** CREATOR;
+}
+EOF
 
